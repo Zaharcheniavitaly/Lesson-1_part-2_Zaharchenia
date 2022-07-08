82cce6cf81458c2ee045f14b6e993d81a8e4538e
Zaharcheniavitaly <zaharchenyavitaly@yandex.by>
2022-07-08 08:54:55 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,3 +3,7 @@
 let productsData = [];
 let productCards = document.querySelectorAll('.catalog__card');
 
+productCards.forEach(productCard => {
+	let name = productCards.querySelectorAll('.')
+})
+
