10797c2dec39a39090d493d470c3cb6a8bea8fa4
Zaharcheniavitaly <zaharchenyavitaly@yandex.by>
2022-07-08 09:12:25 +0300

    update

--- js/index.js
+++ js/index.js
@@ -4,6 +4,10 @@ let productsData = [];
 let productCards = document.querySelectorAll('.catalog__card');
 
 productCards.forEach(productCard => {
-	let name = productCards.querySelectorAll('.')
-})
+	let name = productCard.querySelector('.catalog__card-title').innerText;
+	let desc = productCard.querySelector('.catalog__card-text').innerText;
+	let price = +productCard.querySelector('.catalog__card-price-value').innerText;
+
+});
+
 
